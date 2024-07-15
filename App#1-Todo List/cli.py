a66b60b99911fcf8d3be96f4beca4584659ce4ec
KillerRhys <56098017+KillerRhys@users.noreply.github.com>
2024-07-14 22:08:24 -0400

    Optimization CLI

--- App#1-Todo List/cli.py
+++ App#1-Todo List/cli.py
@@ -25,7 +25,7 @@ def load_data():
         with open("data/fin.dat", "r+") as fin_file:
             completed = fin_file.readlines()
             for item in completed:
-                fin_list.append(item)
+                fin_list.append(item.strip("\n"))
     except FileNotFoundError:
         with open("data/fin.dat", "w+") as fin_file:
             fin_file.write("")
@@ -35,12 +35,19 @@ def load_data():
 # Save Data function
 def save_data():
     with open("data/todo.dat", "w+") as todo_file:
-        for item in todo_list:
-            todo_file.write(f"{item}\n")
+        if not todo_list:
+            pass
+        else:
+            for item in todo_list:
+                todo_file.write(f"{item}\n")
 
     with open("data/fin.dat", "w+") as fin_file:
-        for item in fin_list:
-            fin_file.write(f"{item}\n")
+        if not fin_list:
+            pass
+
+        else:
+            for item in fin_list:
+                fin_file.write(f"{item}\n")
 
 
 def show_items():
@@ -62,43 +69,50 @@ load_data()
 
 # main logic.
 while is_running:
-    user_operation = input("Please enter a command, Add, Show, Edit, Delete, Complete: ")
+    show_items()
+    user_operation = input("Please enter a command, Add, Edit, Delete, Complete, Clear: ").title()
 
-    match user_operation.title():
+    if "Add" in user_operation:
+        new_item = user_operation[4:]
+        todo_list.append(new_item)
+        print(f"{new_item} successfully added to list! \n")
+        save_data()
 
-        case "Add":
-            new_item = input("What would you like to add? ")
-            todo_list.append(new_item)
-
-        case "Show":
-            show_items()
-
-        case "Edit":
-            show_items()
-
-            edit_item = input("Which item would you like to edit?")
-            if len(todo_list) >= int(edit_item):
-                update_item = input("Please type you edits: ")
-                todo_list[int(edit_item)] = update_item
-
-            else:
-                print("That's not a valid selection!")
-
-        case "Delete":
-            show_items()
-
-            target_item = input("Select item to delete: ")
-            if len(todo_list) >= int(target_item):
-                todo_list.pop(int(target_item))
-
-        case "Complete":
-            show_items()
-
-            fin_item = input("Which item would you like to check off?")
-            if len(todo_list) >= int(fin_item):
-                fin_list.append(todo_list[int(fin_item)])
-                todo_list.pop(int(fin_item))
-
-        case "Exit":
+    elif "Edit" in user_operation:
+        edit_item = int(user_operation[5:])
+        if len(todo_list) >= int(edit_item):
+            before = todo_list[int(edit_item)]
+            update_item = input("Please type you edits: ")
+            after = update_item
+            todo_list[int(edit_item)] = update_item
+            print(f"Todo: {before}, was changed to {after}.\n")
             save_data()
-            exit()
+
+        else:
+            print("That's not a valid selection!")
+
+    elif "Delete" in user_operation:
+        target_item = int(user_operation[7:])
+        if len(todo_list) >= int(target_item):
+            print(f"{todo_list[int(target_item)]} has been removed!\n")
+            todo_list.pop(int(target_item))
+            save_data()
+
+    elif "Clear" in user_operation:
+        for item in fin_list:
+            fin_list.clear()
+
+    elif "Complete" in user_operation:
+        fin_item = int(user_operation[9:])
+        if len(todo_list) >= int(fin_item):
+            print(f"{todo_list[int(fin_item)]} has been checked off!\n")
+            fin_list.append(todo_list[int(fin_item)])
+            todo_list.pop(int(fin_item))
+            save_data()
+
+    elif "Exit" in user_operation:
+        save_data()
+        exit()
+
+    else:
+        print("Wrong selection! Try again..\n}")
