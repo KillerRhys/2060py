6855bab9b58aeb53277cc8e63c04e21cc10405cd
KillerRhys <56098017+KillerRhys@users.noreply.github.com>
2024-07-29 15:06:26 -0400

    outline desktop app

--- App#1-Todo List/desktop.py
+++ App#1-Todo List/desktop.py
@@ -2,7 +2,7 @@
     Coded by TechGYQ
     www.mythosworks.com
     OC:2024.07.27(1558) """
-
+import tkinter
 # Imports
 from tkinter import *
 from logic import Logic
@@ -15,5 +15,14 @@ display.title("Tech's Todos")
 display.minsize(width=800, height=800)
 display.configure(bg='Black')
 
+# Item label & settings
+default_x = 0
+default_y = 0
+current_x = 0
+current_y = 0
+item = tkinter.StringVar()
+item.set("Test")
+item_label = tkinter.Label(display, textvariable=item, anchor=tkinter.CENTER, font=("Arial", 16, "bold"), bg="black", fg="red")
+item_label.grid()
 
 display.mainloop()
