10e0e57710cf35a655a0147bc105f1cd614ca27a
KillerRhys <56098017+KillerRhys@users.noreply.github.com>
2024-09-23 20:31:09 -0400

    ignore file fix

--- App#1-Todo List/cli.py
+++ App#1-Todo List/cli.py
@@ -1,3 +1,6 @@
 import logic
 
 brain = logic.Logic()
+while brain.running:
+    brain.write_todos("walk the dog")
+    brain.get_todos()
--- App#1-Todo List/logic.py
+++ App#1-Todo List/logic.py
@@ -1,11 +1,11 @@
 # Imports
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 import time
+import json
 
 
 # Vars
+FILEPATH = "/Data/todo.json"
 CURRENT_TIME = time.strftime("%Y.%B.%d - %H:%M:%S")
 APP_INFO = """ 
                Get'R Done (To-Do App)
@@ -15,38 +15,33 @@ APP_INFO = """
             """
 
 
-# Database creation & setup if not found.
-class Base(DeclarativeBase):
-    pass
-
-
-db = SQLAlchemy(model_class=Base)
-
-
-class Task(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True, unique=True)
-    status: Mapped[bool]
-    name: Mapped[str] = mapped_column(primary_key=True)
-    begin: Mapped[str]
-    finish: Mapped[str]
+# TODO: Database creation & setup if not found.
 
 
 # Main logic for all versions of app.
-class Logic(Flask):
+class Logic:
     def __init__(self):
         super(Logic).__init__()
         self.app = Flask(__name__)
-        self.app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///Data/TODO.db"
-        db.init_app(self.app)
-        with self.app.app_context():
-            db.create_all()
-        self.todos = {}
-        self.complete = {}
+        self.running = True
+        self.todos = []
+        self.complete = []
 
     # Prints app info.
     @staticmethod
     def app_info():
         print(APP_INFO)
 
-    # Grab stored data for to-do's & completed tasks.
+    # TODO: Grab stored data for to-do's & completed tasks.
+    def get_todos(self):
+        for item in self.todos:
+            print(item["start"], ":", item['name'], " Status:", item['status'] )
 
+    # TODO: Write todos to db.
+    def write_todos(self, todo):
+        task = {
+            "start": CURRENT_TIME,
+            "name": todo,
+            "status": False
+        }
+        self.todos.append(task)
